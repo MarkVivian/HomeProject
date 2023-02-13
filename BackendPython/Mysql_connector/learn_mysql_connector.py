ff2155b77eccd6fd8e2ec4765d0f51f8bda64c6d
Mark Vivian <meliodasdragonsin008@gmail.com>
2023-02-13 22:42:50 +0300

    i tested a mysql connector to the python backend

---
+++ BackendPython/Mysql_connector/learn_mysql_connector.py
@@ -0,0 +1,27 @@
+# we are going to use a package
+import mysql.connector
+
+# then we are going to connect to the database.
+my_database = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="M6a2r7k5",
+    database="HouseFinder"
+)
+
+# let's check if the connection has been done
+if my_database.is_connected():
+    print("the database is connected")
+else:
+    print("an error occurred")
+
+# let's create a cursor which will allow us to give commands to mysql.
+cursor = my_database.cursor()
+
+# let's give the database a command
+cursor.execute("SHOW TABLES")
+
+# let's get the results from the executed command
+results = cursor.fetchall()
+
+print(results)
