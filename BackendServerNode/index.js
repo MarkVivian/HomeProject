7f7dc1fca01aba5e27d32a7f7bd6d39ce7549421
Mark Vivian <meliodasdragonsin008@gmail.com>
2023-02-23 07:41:54 +0300

    read and understood the syntax of server tested the api using pacman

--- BackendServerNode/index.js
+++ BackendServerNode/index.js
@@ -3,11 +3,17 @@ const Cors = require("cors")
 
 const App = Express()
 
+App.use(Cors())
+
 App.get('/', (req, res) => {
     res.send("hello wolrd")
 })
 
+App.post("/", (req, res)=>{
+    const Data = req.body
+    res.send(JSON.stringify({message : "the data has been recieved"}))
 
+})
 
 App.listen(3000, ()=>{
     console.log("the port is running")
