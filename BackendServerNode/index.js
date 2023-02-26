ddd4ca2d32ee3cd8fe11442d809b277cbc2223df
Mark Vivian <meliodasdragonsin008@gmail.com>
2023-02-26 15:05:13 +0300

    added a router button

--- BackendServerNode/index.js
+++ BackendServerNode/index.js
@@ -1,13 +1,16 @@
 const Express = require("express")
 const Cors = require("cors")
-const users = require("./Data/users.json")
 const slider_images = require("./Data/slider_images.json")
+const users = require("./Data/users.json")
+const houses = require("./Data/houses.json")
+
 
 const App = Express()
-
 App.use(Cors())
 App.use(Express.json())
 
+
+                //FOR FETCH REQUESTS
 App.get("/slider_images", (req, res)=>{
     res.send(slider_images)
     console.log("the slider_images has been sent")
@@ -18,6 +21,7 @@ App.get("/users", (req, res)=>{
     console.log("the data has been sent")
 })
 
+
 App.post("/newUsers", (req, res)=>{
     const data = req.body
     console.log("data received is ", data)
@@ -25,26 +29,14 @@ App.post("/newUsers", (req, res)=>{
     WriteDataJson(data, "./Data/users.json")
 })
 
-function WriteDataJson(values, path){
-    const fs = require("fs");
-
-    // Load the existing JSON file data
-    const existingData = fs.readFileSync(path, 'utf-8');
-
-    // Parse the JSON data into a JavaScript object
-    const jsonData = JSON.parse(existingData);
-
-    // Modify the object with the new data
-    jsonData.push(values);
-
-    // Convert the modified object back into a JSON string
-    const updatedData = JSON.stringify(jsonData, null, 2);
-
-    // Write the new JSON string to the file system
-    fs.writeFileSync(path, updatedData, 'utf-8');
-}
+App.get("/houses", (req, res)=>{
+    res.status(200).send(houses)
+    console.log("the houses have been sent")
+})
 
 
+
+                  //TEST THE CONNECTION
 App.listen(3000, ()=>{
     console.log("the port is running")
 })
